3e89b0edd76e683d8609256e6bfefdf30b0f7d86
norwil <norweg@st.wmi.amu.edu.pl>
2021-03-13 17:08:00 +0100

    commit1

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+
+    final static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        logger.info("Welcome to Programming Laboratory");
     }
 }
